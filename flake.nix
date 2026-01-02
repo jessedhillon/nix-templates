d228fdcd1bd617227e1592791106aaedafb02098
Jesse Dhillon <jesse@dhillon.com>
2026-01-01 22:39:24 -0800

    feat(dev): add devshell, nix fmt pre-commit hook

--- flake.nix
+++ flake.nix
@@ -1,12 +1,43 @@
 {
+  inputs = {
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs";
+  };
+
   description = "Basic project templtates";
 
-  outputs = { self }: {
-    templates = {
-      python-basic = {
-        path = ./python-basic;
-        description = "Basic Python project scaffolding";
+  outputs =
+    { devshell, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+      devShells.${system}.default =
+        let
+          pkgs = import nixpkgs {
+            system = system;
+            config.allowUnfree = true;
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          name = "nix-config";
+          motd = "{32}nix-config activated{reset}\n$(type -p menu &>/dev/null && menu)\n";
+
+          packages = with pkgs; [
+            pre-commit
+          ];
+        };
+
+      templates = {
+        python-basic = {
+          path = ./python-basic;
+          description = "Basic Python project scaffolding";
+        };
       };
     };
-  };
 }
